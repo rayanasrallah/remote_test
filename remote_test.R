f32896e48e1ee20cd4570ab504de864518421570
rayanasrallah <raya.nasrallah@mail.mcgill.ca>
2021-10-06 10:29:16 -0400

    Changes made after generating a token

---
+++ remote_test.R
@@ -0,0 +1,12 @@
+#loading a new package that allows you to manage tokens or passwords
+
+install.packages(gitcreds)
+library("gitcreds")
+gitcreds_get()
+gitcreds_set()
+2
+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
+
+
+#lets make changes and stage+commit
+#now gitHub should not ask for access
