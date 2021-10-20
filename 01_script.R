8128334bf08366308d52f0bfa8872dd5b16bc511
rayanasrallah <raya.nasrallah@mail.mcgill.ca>
2021-10-20 10:55:39 -0400

    new numbers

--- 01_script.R
+++ 01_script.R
@@ -1,9 +1,9 @@
 #Session 7, Oct. 20, 2021
 12345
 #initial commit
-
+0000000000000000
 #first change to the main branch - does in conflict now?
-
+454545454545
 #second change: creating a conflict - does in conflict now? again and again
 potatoooo
 6666666666666
