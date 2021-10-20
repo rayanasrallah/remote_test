56019893cbe8d7bb932c685c3f1fa8921be02a5e
rayanasrallah <raya.nasrallah@mail.mcgill.ca>
2021-10-20 10:47:05 -0400

    deleting line

--- 01_script.R
+++ 01_script.R
@@ -6,4 +6,4 @@
 
 #second change: creating a conflict - does in conflict now?
 
-#to create a conflict, the changes should be done on the same file (same line) in the two branches (the main and the feature branch) does in conflict now?
+#deleted line
